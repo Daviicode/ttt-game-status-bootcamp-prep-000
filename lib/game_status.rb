f16f0c338eb7467a128b85a0c46d949f91e4294f
David zhang <d.ydzdavid@yahoo.com>
2019-03-01 22:20:54 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,8 +25,19 @@ def won?(board)
     else if combo.full? {|position| board[position] == "X"} || combo.full? {|position| board[position] == "O"}
       return winner = combo
       end
-      winner
     end 
+    winner
 end
 
+def full?(board)
+  board.all? {|index| index == "X" || index == "O"}
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
 
